4c8fa4a2e6de4e2ca5ccb3906c1920aa358eb6d9
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-24 17:21:53 -0400

    update files

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -7,17 +7,17 @@
  *
  * Return: pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_s *binary_tree_node(binary_tree_s *parent, int value)
 {
-    binary_tree_t *new_node;
+    binary_tree_s *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
+    new_node = malloc(sizeof(binary_tree_s));
     if (new_node == NULL)
     {
         return (NULL);
     }
 
-    new_node->value = value;
+    new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
     new_node->right = NULL;
