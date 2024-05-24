756a988c18af0f5c9d1ae032640286bb6f3b784c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-24 17:36:01 -0400

    Error fixing

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -7,11 +7,11 @@
  *
  * Return: pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(struct binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(struct binary_tree_s));
+    new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
     {
         return (NULL);
