23a5645f4ccd732381f2fc4b25ee84b0274ea044
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-24 17:12:08 -0400

    Adds bynary tree task

---
+++ heap_insert/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+#include <stdlib.h>
+
+typedef struct binary_tree_s
+{
+    int value;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
