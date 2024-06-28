b50fa9b93fbf8d599752308b4bd29c8ccaef2b25
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-06-28 12:03:23 -0400

    Fix comments

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -12,15 +12,15 @@ int check_cycle(listint_t *list) {
     listint_t *slow = list, *fast = list;
 
     while (fast != NULL && fast->next != NULL) {
-        slow = slow->next; // Move slow pointer by one
-        fast = fast->next->next; // Move fast pointer by two
+        slow = slow->next; /* Move slow pointer by one */
+        fast = fast->next->next; /* Move fast pointer by two*/
 
         if (slow == fast) {
-            // The pointers have met, there is a cycle
+            /* The pointers have met, there is a cycle*/
             return 1;
         }
     }
 
-    // If we've reached here, fast has encountered NULL, no cycle
+    /* If we've reached here, fast has encountered NULL, no cycle*/
     return 0;
 }
