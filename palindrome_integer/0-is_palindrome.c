625fbf1c2c9ad56d58d8e3736270363bccdaedda
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-07-05 17:32:38 -0400

    update code

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -1,24 +1,22 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "palindrome.h"
 
 /**
- * is_palindrome - Check if a number is a palindrome
+ * is_palindrome - Check if a given number is a palindrome
  *
  * @n: Number to check
  *
  * Return: 1 if n is a palindrome, 0 otherwise
  */
-int is_palindrome(unsigned long n){
-    char str[20];
-    int i;
-    int len;
-    sprintf(str, "%lu", n);
-    len = strlen(str);
-    for (i = 0; i < len / 2; i++){
-        if (str[i] != str[len - i - 1]){
-            return 0;
-        }
+int is_palindrome(unsigned long n)
+{
+    unsigned long reversed = 0, remainder, original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
     }
-    return 1;
+
+    return (original == reversed);
 }
